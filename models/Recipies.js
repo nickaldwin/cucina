7b162ebafe3673112eef9236121cffd8d432a64a
nickaldwin <nicklemoncito98@gmail.com>
2023-09-20 20:25:16 +0800

    update the models

--- models/Recipies.js
+++ models/Recipies.js
@@ -9,22 +9,50 @@ const RecipieSchema = new mongoose.Schema({
     type: String,
     require: true,
   },
+
+  country:{
+    type: String,
+    require: true,
+  },
+  
+  dish:{
+    type: String,
+    require: true,
+  },
+  
+  ingredients:{
+    type: [String],
+    require: true,
+  },
+
+  directions:{
+    type: [String],
+    require: true,
+  },  
+  
   cloudinaryId: {
     type: String,
     require: true,
   },
+
   caption: {
     type: String,
     required: true,
   },
+
   likes: {
     type: Number,
+    required: true,
+  },
+  bookmarks:{
+    type: Array,
     required: true,
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+
   createdAt: {
     type: Date,
     default: Date.now,
